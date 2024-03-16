b184541943caf9753b598a7d2ad01c578c9c214a
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-03-16 12:03:03 +0700

    + firebase suport https://firebase.google.com/docs/flutter/setup?hl=en&authuser=0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_hosting/firebase_options.dart';
 import 'package:firebase_hosting/my_app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
