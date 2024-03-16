a9522f38141377c8e8045f4682baaf6d6a0d20c6
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-03-16 11:41:10 +0700

    + init flutter source

---
+++ android/app/src/main/kotlin/com/lcdsoft/flutter/firebase_hosting/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lcdsoft.flutter.firebase_hosting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_hosting/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
