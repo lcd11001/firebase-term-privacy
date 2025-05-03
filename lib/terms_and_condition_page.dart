6e6a747dfbe6d7880876ba034b2d968c760e357d
LCD-PC <lcd11001@gmail.com>
2025-05-03 23:31:18 +0700

    - update l10n

--- lib/terms_and_condition_page.dart
+++ lib/terms_and_condition_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_hosting/components/heading.dart';
 import 'package:firebase_hosting/components/hyperlink.dart';
 import 'package:firebase_hosting/components/paragraph.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:firebase_hosting/l10n/generated/app_localizations.dart';
 
 class TermsAndConditionPage extends StatefulWidget {
   final String appTitle;
